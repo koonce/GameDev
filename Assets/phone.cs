36c2ae0e8de33f2c92037bdbf2ff7df7a230cdf8
Unknown <erk347@nyu.edu>
2017-02-01 16:18:24 -0500

    Put Unity project  into Git Repository

---
+++ Assets/phone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phone : MonoBehaviour {
+
+    bool phoneUp = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && phoneUp)
+        {
+            phoneUp = false;
+            Vector3 location = transform.position;
+            location.y = -10f;
+            transform.position = location;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !phoneUp)
+        {
+            phoneUp = true;
+            Vector3 location = transform.position;
+            location.y = 0f;
+            transform.position = location;
+        }
+	}
+}
