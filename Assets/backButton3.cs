e5c4086d01fd7f9b8be823be38d6a06efa01740a
Unknown <erk347@nyu.edu>
2017-02-13 01:45:59 -0500

    start of music app

---
+++ Assets/backButton3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backButton3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (GetComponent<Collider2D>().OverlapPoint(mouseWorldPos))
+            {
+                Debug.Log("go back from music");
+                GameObject screen = GameObject.Find("musicScreen");
+                Transform mPos = screen.GetComponent<Transform>();
+                Vector3 mPosi = mPos.position;
+                mPosi.z = 1f;
+                mPos.position = mPosi;
+            }
+        }
+    }
+}
---
+++ Assets/musicalButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicalButton : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (GetComponent<Collider2D>().OverlapPoint(mouseWorldPos))
+            {
+                Debug.Log("click music");
+                GameObject musicScreen = GameObject.Find("musicScreen");
+                Transform tPos = musicScreen.GetComponent<Transform>();
+                Vector3 tPosi = tPos.position;
+                tPosi.z = -5f;
+                tPos.position = tPosi;
+            }
+        }
+    }
+}
