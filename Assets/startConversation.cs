0c643e3bb6b1c58442c5cc06583c5d878e26bd0d
Unknown <erk347@nyu.edu>
2017-02-27 03:27:07 -0500

    start of people conversations

---
+++ Assets/startConversation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startConversation : MonoBehaviour
+{
+
+    public GameObject speech;
+    bool cannotTalk;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cannotTalk = GameObject.Find("Phone").GetComponent<phone>().phoneUp;
+
+        if (!cannotTalk)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                if (GetComponent<BoxCollider2D>().OverlapPoint(mouseWorldPos))
+                {
+                    Debug.Log("stranger is talking");
+                    speech.SetActive(true);
+                }
+            }
+        }
+    }
+}
